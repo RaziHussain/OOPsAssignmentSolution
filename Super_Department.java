987239a92f8e4681db2af874793e52549c249567
RaziHussain <94157368+RaziHussain@users.noreply.github.com>
2021-11-25 13:05:58 +0530

    Add files via upload

---
+++ Super_Department.java
@@ -0,0 +1,21 @@
+package OOPsAssignmentSolution1;
+
+public class Super_Department {
+
+	String departmentName() {
+		return "Super Department";
+	}
+
+	String getTodayWork() {
+		return "No Work As of Now";
+	}
+
+	String getWorkDeadline() {
+		return " Nil";
+	}
+
+	String isTodayAHoliday() {
+		return " Today is Not a Holiday";
+	}
+}
+
